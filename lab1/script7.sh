5f31868ea4faa158a11c21a79e85a1c145846cf2
The-Macallan <65563344+The-Macallan@users.noreply.github.com>
2020-09-28 02:48:08 +0300

    Update script7.sh

--- lab1/script7.sh
+++ lab1/script7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -sIEhro "[[:alnum:]._-]+@[[:alnum:]]+\.[[:alpha:]]+" //etc/* | tr "\n" ", " > emails.lst
+grep -sIEhro "[[:alnum:]._-]+@[[:alnum:]]+.[[:alpha:]]+" /etc/* | tr "\n" ", " > emails.lst
